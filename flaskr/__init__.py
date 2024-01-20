e52a415431a33fb5b3caee336de5780c24668b94
parkrafael <parkrafael@gmail.com>
2024-01-20 15:53:37 -0800

    added stylesheet

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, static_folder='static')
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -38,7 +38,7 @@ def create_app(test_config=None):
 
     from . import main
     app.register_blueprint(main.bp)
-    
+
     app.add_url_rule('/', endpoint='index')
 
     return app
