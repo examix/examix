ef38868c849a8335c6267704a95a5b1fb50939fb
parkrafael <parkrafael@gmail.com>
2024-01-20 14:05:07 -0800

    restructuring of files

--- backend/blog.py
+++ backend/blog.py
@@ -3,8 +3,8 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from backend.auth import login_required
+from backend.db import get_db
 
 bp = Blueprint('blog', __name__)
 
