cff2a04ad0b7c332ab655839ac77d6938964d24c
Johnathan Warawa <johnnyw@johnnyw.ca>
2024-01-21 10:29:58 -0800

    style improvements for remix function

--- flaskr/remix_functions.py
+++ flaskr/remix_functions.py
@@ -42,32 +42,22 @@ def remix(exp_time, diff, school="uvic", department="CSC", course_code="110"):
 
     questions = db.get_questions_db()
 
-    print("QUESTIONS")
-    for question in questions:
-        print(question[0] + "\n\n")
-
     seed_q = questions.pop(random.randint(0, len(questions) - 1))
-    time = seed_q[7]
+    time = seed_q['duration']
     time_diff = calc_time_diff(time, exp_time)
-    cur_difficulty = seed_q[1]
+    cur_difficulty = seed_q['difficulty']
     results = [seed_q]
 
-    while time_diff > 0.2 or time <= exp_time:
+    while time_diff > 0.2 and time <= exp_time:
         singles = []
         doubles = []
         for question in questions:
             cur_deiv, new_avg =  calc_dieviation([question], cur_difficulty, time, diff)
             singles.append( ([question], cur_deiv, new_avg) )
-            print("QUESTION: %s, dur %s, diff %s, diev %s, new avg %s" %(question[0][:10], question[7], question[1], cur_deiv, new_avg))
-        #singles = [([question], *calc_dieviation([question], cur_difficulty, time, exp_time)) for question in questions]
 
         for q_pair in itertools.product(questions, questions):
             cur_deiv, new_avg =  calc_dieviation(q_pair, cur_difficulty, time, diff)
             doubles.append( (q_pair, cur_deiv, new_avg) )
-            print("QUESTION: %s %s, dur %s %s, diff %s %s, diev %s, new avg %s" %(q_pair[0][0][:10], q_pair[0][7], q_pair[0][1], q_pair[1][0][:10], q_pair[1][7], q_pair[1][1], cur_deiv, new_avg))
-            
-        #doubles = [(qs, calc_dieviation(qs, cur_difficulty, len(results), diff))  for qs in itertools.product(questions, questions)]
-        doubles = []
 
         candidates = sorted(singles + doubles, key=lambda x: x[1])
 
@@ -78,7 +68,7 @@ def remix(exp_time, diff, school="uvic", department="CSC", course_code="110"):
         while candidates and new_time > exp_time*ACCEPTABLE_ERROR:
             candidates.pop(0) 
             new_time = time + sum([q[1] for q in candidates[0][0]] if candidates else [])
-       
+
         if not candidates:
             break
         
@@ -99,5 +89,3 @@ def remix(exp_time, diff, school="uvic", department="CSC", course_code="110"):
     print(dir(results[0]))
     results_dict = [  { key : result[key] for key in result.keys() } for result in results  ]
     return results_dict, time, cur_difficulty
-    #return "Qs: %s, time: %s" % ([result[0] for result in results], time)
-        
