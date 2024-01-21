db8ac73edba44a1ec40a61e70359c4e8a755259a
liamshatzel <liam.shatzel@gmail.com>
2024-01-20 21:14:11 -0800

    question object

--- flaskr/exam.py
+++ flaskr/exam.py
@@ -20,6 +20,15 @@ class Page:
 
 
 class Question:
-    
-    def __init__(self, num):
-        self.__num = num
+    def __init__(self, question_text, difficulty, page_num, vertices, question_type, num_points, exam_image, duration,
+                 answer=None):
+        self.question_text = question_text
+        self.difficulty = difficulty
+        self.page_num = page_num
+        self.vertices = vertices
+        self.question_type = question_type
+        self.num_points = num_points
+        self.exam_image = exam_image
+        self.duration = duration
+        self.answer = answer
+
