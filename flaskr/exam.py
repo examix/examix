cfe47034901fe3bece60369da3c902cec2aac9b5
liamshatzel <liam.shatzel@gmail.com>
2024-01-20 22:01:14 -0800

    fixed sql, foreign keys working

--- flaskr/exam.py
+++ flaskr/exam.py
@@ -1,19 +1,20 @@
 class Exam:
-    def __init__(self, num_pages, difficulty, prof, pdf_name, time, date, exam_type, num_questions, pages):
+    def __init__(self, num_pages, difficulty, prof, pdf_name, duration, date, exam_type, num_questions, pages, school):
         self.num_pages = num_pages
         self.difficulty = difficulty
         self.prof = prof
         self.pdf_name = pdf_name
-        self.time = time
+        self.duration = duration
         self.date = date
         self.exam_type = exam_type
         self.num_questions = num_questions
         self.pages = pages
+        self.school = school
 
 
 class Page:
-    def __init__(self, num, width, height, questions=None):
-        self.num = num
+    def __init__(self, page_num, width, height, questions=None):
+        self.page_num = page_num
         self.width = width
         self.height = height
         self.questions = questions
